c18f3ccb00eb40f4b105ff152277c7bc960425c8
laonmun <laonm@khu.ac.kr>
2022-11-27 21:03:39 +0900

    mysql and node.js

---
+++ test/models/index.js
@@ -0,0 +1,11 @@
+const Sequelize = require('sequelize');
+
+const env = process.env.NODE_ENV || 'development';
+const config = require(__dirname + '/../config/config.json')[env];
+const db = {};
+
+const sequelize = new Sequelize(config.database, config.username, config.password, config);
+
+db.sequelize = sequelize;
+
+module.exports = db;
