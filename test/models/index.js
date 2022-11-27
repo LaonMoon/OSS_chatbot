887d58791aad7d658bee17e8736a6633d918d314
3un0ia <xxxx83145@gmail.com>
2022-11-27 22:53:26 +0900

    edited

--- test/models/index.js
+++ test/models/index.js
@@ -1,4 +1,6 @@
 const Sequelize = require('sequelize');
+const Menu = require('./menu');
+const Review = require('./review');
 
 const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/config.json')[env];
@@ -7,5 +9,14 @@ const db = {};
 const sequelize = new Sequelize(config.database, config.username, config.password, config);
 
 db.sequelize = sequelize;
+
+db.Menu = Menu;
+db.Review = Review;
+
+Menu.init(sequelize);
+Review.init(sequelize);
+
+Menu.associate(db);
+Review.associate(db);
 
 module.exports = db;
