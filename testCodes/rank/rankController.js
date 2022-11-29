ef61eefeb02ffb8d114a1879051b0cdb1d4c12a3
d-amor <rew1212@naver.com>
2022-11-29 14:16:00 +0900

    2022-11-29 2:14 review v.2( +validation ) by jaeeun

--- testCodes/rank/rankController.js
+++ testCodes/rank/rankController.js
@@ -10,15 +10,25 @@ export const postReviews = async(req,res)=>{
 
     const {menu,menu_rank,menu_description,userID}=req.body;
 
-    if(!(menu=="중식1"||menu=="중식2"||menu=="석식"))
-        return res.send("다시 입력하시오");
-
     if(!menu)
-        return res.send("menu의 종류를 입력하세요");
+        return res.send("menu의 종류를 입력하세요(중식1(소반),중식2(특식),석식)");
+
+    if(!(menu=="중식1"||menu=="중식2"||menu=="석식"))
+        return res.send("중식1,중식2,석식 중에 다시 입력하시오");
+    
     if(!menu_rank)
-        return res.send("menu의 rank를 입력하세요");
+        return res.send("menu의 점수를 0부터5까지 정수형으로 입력하세요");
+    
+    if(menu_rank<0||menu_rank>5)
+        return res.send("menu의 점수는 0이상, 5이하의 정수형태로 입력해주세요");
+
+    if(!Number.isInteger(menu_rank))
+        return res.send("menu의 점수는 정수형으로 입력해주세요");
+
+    /*
     if(!userID)
         return res.send("userid입력오류");
+        */
     
     const postReviewResponse = await createreview(menu,menu_rank,menu_description,userID);
 
