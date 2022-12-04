f04bd9e17a7c9a312f785627aac74af3025f6bf1
EC2 Default User <ec2-user@ip-172-31-13-39.ec2.internal>
2022-12-04 08:37:56 +0000

    Add about.js ver1

--- methods/about.js
+++ methods/about.js
@@ -1,5 +1,5 @@
 const Client = require('@line/bot-sdk').Client
-const TOKEN = '...'
+const TOKEN = 'hq1nOJuDiR8Cnx6eEySETfwO0KbrAXBW+1PSA3lLJu1fNF1eNilPPs0hMsQgTOZ0XnkmSCcFAFufZdLVJYcWIYbTkf6EaQsCCK6t0myvEk5eBlsi+oGAeAWOnlEukkiOf4OXgSKDoc3WjuCP6Cq01QdB04t89/1O/w1cDnyilFU='
 
 const User = require('../models/user').User
 
@@ -20,15 +20,19 @@ function post_about(eventObj) {
         //     "type":"text",
         //     "text":"누적 사용자 수 : %num"%total
         // },        
-        // DB에서 오늘의 식단 리뷰 정보 가져오기    
-        {
-            "type":"text",
-            "text":"오늘의 식단 리뷰 수 :"
-        },
+        // // DB에서 오늘의 식단 리뷰 정보 가져오기    
+        // {
+        //     "type":"text",
+        //     "text":"오늘의 식단 리뷰 수 :"
+        // },
         // 그 외 정보
         {
             "type":"text",
             "text":"2022 오픈소스SW개발 팀프로젝트로 진행되었으며, 서비스 시작일은 12월 3일입니다."
+        },
+        {
+            "type":"text",
+            "text":"더 자세한 명령어에 대한 내용은 'help' 명령어를 이용해주세요."
         }
     ]
 
