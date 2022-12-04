0967ae3848908889200b1c2842f18a30ce8a5747
EC2 Default User <ec2-user@ip-172-31-13-39.ec2.internal>
2022-12-04 07:32:07 +0000

    Add eventHandler.js

--- methods/eventHandler.js
+++ methods/eventHandler.js
@@ -14,17 +14,19 @@ function handleEvent(event) {
     }
     // MESSAGE EVENT
     if (event.type === "message") {
+
+        console.log(event.source.userId)
         const message = event.message.text
         // cases
         switch(message) {
             case ('help'  || '도움말' || '명령어'): {message_help(event); break;}
-            case ('about' || '서비스 소개'): {post_about(eventObj.replyToken, eventObj); break;}
+            case ('about' || '서비스 소개'): {post_about(event); break;}
             case ('오늘 메뉴 알려줘' || '내일 메뉴 알려줘'
                     || '이번 주 메뉴 알려줘'): {break;}
             case ('리뷰 작성'): {break;}
             case ('오늘 학식 어때'): {break;}
             case ('메뉴 지정'): {break;}
-            case ('알람 설정'): {Alarm_Handler(eventObj); break;}
+            case ('알람 설정'): {Alarm_Handler(event); break;}
         }
     }
 }
