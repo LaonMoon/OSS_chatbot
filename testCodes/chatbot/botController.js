441546bcbcf0ce360fc8952d238fbbc2209dfe57
EC2 Default User <ec2-user@ip-172-31-32-27.ap-northeast-2.compute.internal>
2022-12-04 08:33:45 +0000

    commit for merge to origin master

--- testCodes/chatbot/botController.js
+++
@@ -1,2 +0,0 @@
-import { createPool } from "./botService";
-
--- testCodes/chatbot/botService.js
+++
@@ -1,9 +0,0 @@
-require("dotenv").config();
-import pool from "../config/database";
-/*
-export const createPool = async()=>{
-    const connection = await pool.getConnection(async conn=>conn);
-    if(connection)
-    console.log("getconnection");
-
-}*/
