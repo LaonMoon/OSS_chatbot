7ef3cf428b4c49886bf7116f2582190ec8b2849e
EC2 Default User <ec2-user@ip-172-31-32-27.ap-northeast-2.compute.internal>
2022-12-08 11:02:48 +0000

    Update review by jaeeun

---
+++ methods/rank/rankService.js
@@ -0,0 +1,39 @@
+import { insertreview,selectReview,selectmenurank,selectmenurankdescription } from "./rankDao";
+import pool from "../../config/database";
+
+export const createreview = async(menu,menu_rank,menu_description,)=>{
+try{
+    const connection = await pool.getConnection(async conn=>conn);
+    
+    const insertreviewParams = [menu,menu_rank,menu_description];
+
+    //await connection.beginTransaction() 트랜잭션 오류나서 일단 pass 근데 하긴 해야할 듯
+    const createreviewResult = await insertreview(connection,insertreviewParams);
+    
+    //await connection.commit()
+    connection.release();
+
+    return createreviewResult;
+
+}catch(err){
+    console.log("createreview error");
+    //await connection.rollback();
+}
+
+}
+
+export const showReview= async(menu)=>{
+    const conn = await pool.getConnection(async(conn)=>conn);
+
+    const selectRankResult = await selectmenurankdescription(conn,menu);
+    conn.release;
+    return selectRankResult;
+}
+
+export const showMenuRank= async(menu)=>{
+    const conn = await pool.getConnection(async(conn)=>conn);
+
+    const selectRankResult = await selectmenurank(conn,menu);
+    conn.release;
+    return selectRankResult;
+}
